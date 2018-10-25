b89298c2f7aaf3737f79cfae8feb48e97144c100
Ayyanchira <ayyanchira.akshay@gmail.com>
2018-10-25 19:42:02 -0400

    Initial setup. Imported Holotoolkit. Added vuforia Camera component, changed Vuforia settings as mentioned on the web. Created NetworkManager component from previous work. Tried adding sound detected when image target detection. Its working. Now need to implement Location transformation that we studied this week

---
+++ Assets/LocationNetworkManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LocationNetworkManager : NetworkManager
+{
+
+    protected static short messageID = 777;
+    public GameObject playerObject;
+
+    public class CustomMessage : MessageBase
+    {
+        public string deviceType, purpose, ipAddress;
+        public Vector3 devicePosition;
+        public Quaternion deviceRotation;
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.IsClientConnected())
+        {
+            if (playerObject == null)
+            {
+                playerObject = GameObject.Instantiate(playerPrefab, new Vector3(5, 1, -6), Quaternion.identity);
+
+            }
+            else
+            {
+                updateLocation();
+            }
+        }
+        else
+        {
+            print("Not connected to server...");
+        }
+    }
+
+    void updateLocation()
+    {
+        var msg = new CustomMessage();
+        msg.devicePosition = playerObject.transform.position;
+        msg.deviceRotation = playerObject.transform.rotation;
+        msg.deviceType = "Hololens";
+        msg.purpose = "Simulation";
+        //#if !UNITY_EDITOR
+        //        Debug.Log("Unity Editor");
+        //        print("Printing the network address as: ' " + GetLocalIPAddress() + "'. Considering this as local ip address");
+        //#endif
+
+        msg.ipAddress = this.networkAddress;
+        client.Send(messageID, msg);
+    }
+
+    //#if !UNITY_EDITOR
+    //    public static string GetLocalIPAddress()
+    //    {
+    //        var host = Dns.GetHostEntry(Dns.GetHostName());
+    //        foreach (var ip in host.AddressList)
+    //        {
+    //            if (ip.AddressFamily == AddressFamily.InterNetwork)
+    //            {
+    //                return ip.ToString();
+    //            }
+    //        }
+    //        throw new Exception("No network adapters with an IPv4 address in the system!");
+    //    }
+    //#endif
+
+    public void ConnectToServer()
+    {
+        StartClient();
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerMovementManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementManager : MonoBehaviour {
+
+    GameObject hololensPlayer;
+
+    public GameObject hololensCamera;
+    public GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (LocationNetworkManager.singleton.IsClientConnected())
+        {
+            if(player == null)
+            {
+                player = GameObject.FindGameObjectWithTag("Player");
+
+            }
+            else
+            {
+                print("Updating player coordinates");
+                if (hololensCamera != null){
+                    player.transform.position = hololensCamera.transform.position;
+                    player.transform.rotation = hololensCamera.transform.rotation;
+                }
+                else
+                {
+                    print("Camera not found.");
+                }
+            }
+        }
+	}
+}
